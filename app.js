02ff1bf5ee5bb843606b7bc4e37f34553928cefc
zhangyuchi <2669327287@qq.com>
2020-05-08 20:03:44 +0800

    add msg

--- app.js
+++ app.js
@@ -4,5 +4,7 @@ const static = require('koa-static');
 const app = new koa()
 app.use(cors())
 app.use(static(__dirname+'/public'));
-app.listen(4000)
+app.listen(4000,()=>{
+	console.log("gis server is running at 4000")
+})
 
